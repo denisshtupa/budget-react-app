3bbfe124f616b86a104b7bddf8597c5c94da6e0d
denis.shtupa <denis.shtupa@3i-solutions.net>
2021-01-18 21:23:16 +0100

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// THIS IS A FUNCTIONAL COMPONENT ( no need to use state, so we use Functional Component instead of cClass Component)
+export const Header = () => {
+    return (
+        <h2>
+            Expense tracker
+        </h2>
+    )
+}
